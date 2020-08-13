540104e0b73723ea6fb2373b713bcae236d6a81d
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-08-13 15:01:12 +0300

    create public and private router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Switch } from 'react-router';
 // import logo from './logo.svg';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
