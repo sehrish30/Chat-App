81c5c04fa33af9c98077a2167538f07827e102de
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-08-16 17:56:25 +0300

    Create layout for page and created chat page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { Grid, Row, Col } from 'rsuite';
+import { Switch, Route, useRouteMatch } from 'react-router';
 import Sidebar from '../../components/Sidebar';
 import { RoomsProvider } from '../../context/rooms.context';
+import Chat from './Chat';
+import { useMediaQuery } from '../../misc/custom-hooks';
 
 const Home = () => {
+  const isDesktop = useMediaQuery(`(min-width: 992px)`);
+
+  const { isExact } = useRouteMatch(); //Checks if we are on the specified loc returns bool
+  const canRenderSidebar = isDesktop || isExact;
+
   return (
     <RoomsProvider>
       <Grid fluid className="h-100">
         <Row className="h-100">
-          <Col xs={24} md={8} className="h-100">
-            <Sidebar />
-          </Col>
+          {canRenderSidebar && (
+            <Col xs={24} md={8} className="h-100">
+              <Sidebar />
+            </Col>
+          )}
+
+          <Switch>
+            <Route exact path="/chat/:chatId">
+              <Col xs={24} md={16} className="h-100">
+                <Chat />
+              </Col>
+            </Route>
+            <Route>
+              {isDesktop && (
+                <Col xs={24} md={16} className="h-100">
+                  <h6 className="text-center mt-page">Please select Chat</h6>
+                </Col>
+              )}
+            </Route>
+          </Switch>
         </Row>
       </Grid>
     </RoomsProvider>
